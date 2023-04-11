3b1c3c7de4dea48bd9f2fec5a7fd5cf40cafb7fd
Jack Luo <jluo@mmcnz.co.nz>
2023-04-11 13:45:26 +1200

    initial commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        // DbSet Activities represents Activities table in our DB
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
